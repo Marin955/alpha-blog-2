856a48590d539321e8db75ae50660138e1afb0d0
marin <marin.bilic55@gmail.com>
2021-01-12 10:34:39 +0100

    initial articles classes

---
+++ db/migrate/20210112092749_init_articles.rb
@@ -0,0 +1,10 @@
+class InitArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
